95b954f95e453964c53afd62e1fc7926b83475df
enicolasgarcia <91081078+enicolasgarcia@users.noreply.github.com>
2026-03-03 00:15:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ if os.path.exists(archivo_db):
     # --------------------------------
     
    st.subheader("📊 Historial General de Registros")
-       df_limpio = df_ver.copy()
+   df_limpio = df_ver.copy()
      columnas_num = ["Inversión", "Costo_Total", "Precio_Seguro_x_Kg"]
     for col in columnas_num:
         df_limpio[col] = df_limpio[col].apply(lambda x: f"$ {x:,.0f}")
