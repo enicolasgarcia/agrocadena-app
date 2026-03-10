cddf0f9e38e99ade5b02377e9c3b76522e4fd92d
enicolasgarcia <91081078+enicolasgarcia@users.noreply.github.com>
2026-03-09 19:56:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ with st.sidebar:
             }])
             
             df_actualizado = pd.concat([df_existente, nueva_fila], ignore_index=True)
-            conn.update(worksheet="Sheet1", data=df_actualizado)
+            conn.create(worksheet="Sheet1", data=df_actualizado)
             st.success("¡Datos guardados!")
             st.balloons()
             st.rerun()
