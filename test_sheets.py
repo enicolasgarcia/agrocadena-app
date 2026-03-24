22746ae37e7b4bf3b19ffbab6560e1f803273a43
enicolasgarcia <91081078+enicolasgarcia@users.noreply.github.com>
2026-03-23 23:23:59 -0500

    Update test_sheets.py

--- test_sheets.py
+++ test_sheets.py
@@ -1,1 +1,14 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
+scope = ["https://spreadsheets.google.com/feeds",
+         "https://www.googleapis.com/auth/drive"]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name("credenciales.json", scope)
+client = gspread.authorize(creds)
+
+sheet = client.open("NOMBRE_DE_TU_SHEET").sheet1
+
+sheet.append_row(["Finca 1", 100, 5000])
+
+print("Datos guardados en Google Sheets ✅")
