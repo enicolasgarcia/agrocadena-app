fc3ef91e714ea5aca2266620cfb82143a6ca12d2
enicolasgarcia <91081078+enicolasgarcia@users.noreply.github.com>
2026-03-26 01:20:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -186,7 +186,7 @@ else:
 
     if ganancia_valor > 0:
         st.success("🟢 Estás generando GANANCIA")
-    elif ganacia_valor < 0:
+    elif Ganacia_valor < 0:
         st.error("🔴 Estás generando PÉRDIDA")
     else:
         st.info("🟡 Estás en punto de equilibrio")
